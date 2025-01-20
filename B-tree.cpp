cc0e5e070dbb89f591c60ad4ce422747eb161599
Anthony <ntd806@gmail.com>
2025-01-20 21:11:03 +0700

    create  b-tree

--- B-tree.cpp
+++ B-tree.cpp
@@ -1,33 +1,68 @@
 #include <iostream>
+#define MAX 1000
 using namespace std;
 
 struct BTree {
-    int *d;              // Con trỏ tới mảng các khóa (keys).
+    int *keys;              // Con trỏ tới mảng các khóa (keys).
     BTree **child_ptr;   // Con trỏ tới mảng các con trỏ (chỉ đến các nút con của nút hiện tại).
-    bool l;              // Cờ (flag) để kiểm tra xem nút này có phải là nút lá hay không.
+    bool flag;              // Cờ (flag) để kiểm tra xem nút này có phải là nút lá hay không.
     int n;               // Số lượng khóa hiện có trong nút.
-} *r = NULL, *np = NULL, *x = NULL;
+};
 
 BTree* init(int order) {
     // Cấp phát bộ nhớ cho một nút mới của cây B-Tree
-    np = new BTree;
+    BTree* newTree = new BTree();
     // Cấp phát mảng các khóa (d) với kích thước order - 1
     // Vì mỗi nút có tối đa order - 1 khóa
-    np->d = new int[order - 1];
+    newTree->keys = new int[order - 1];
     // Cấp phát mảng con trỏ đến các nút con (child_ptr)
     // Mỗi nút có tối đa order con trỏ con, bao gồm cả nút lá và nút không lá
-    np->child_ptr = new BTree *[order];
+    newTree->child_ptr = new BTree *[order];
     // Đặt cờ l = true, chỉ ra rằng đây là một nút lá (leaf node)
-    np->l = true;
+    newTree->flag = true;
     // Đặt số lượng khóa trong nút là 0 (chưa có khóa)
-    np->n = 0;
+    newTree->n = 0;
     // Khởi tạo tất cả các con trỏ trong mảng child_ptr là NULL
     // Các con trỏ này trỏ đến các nút con và sẽ được gán giá trị khi cần thiết
     for (int i = 0; i < order; i++) {
-        np->child_ptr[i] = NULL;
+        newTree->child_ptr[i] = NULL;
     }
     // Trả về con trỏ đến nút vừa khởi tạo
-    return np;
+    return newTree;
+}
+
+bool checkChildCount(BTree* node, int order) {
+    if(node != NULL) return true;
+
+    int minChild = order;
+    int maxChild = 2 * order;
+    bool valid = node->n >= minChild && node->n <= maxChild;
+
+    if(valid) return true;
+
+    return false;
+}
+
+struct Stack {
+    BTree **a;  // Con trỏ động đến mảng con trỏ đến các nút
+    int top;    // Vị trí đỉnh của stack
+};
+
+void init(Stack* &s) {
+    s->a = new BTree *[MAX];
+    s->top = -1;
+}
+
+void push(Stack* &s, BTree *x) {
+    s->a[++s->top] = x;
+}
+
+BTree* pop(Stack* &s) {
+    return s->a[s->top--];
+}
+
+void insert(BTree* &root, int x, Stack* &s ) {
+    if (root == NULL ) return;
 }
 
 int main() {
