521540d7d99042aac68e198ea749abed522427f1
Anthony <ntd806@gmail.com>
2025-02-08 15:17:10 +0700

    add list into graph

--- graph.cpp
+++ graph.cpp
@@ -2,13 +2,40 @@
 #define MAX 20
 using namespace std;
 
-struct graph {
+struct Graph {
     int vertex;
     int matrix[MAX][MAX];
 };
 
+struct List
+{
+    int data[MAX];
+    int size;
+};
+
+void initList(List &l) {
+    l.size = 0;
+}
+
+void addList(List &l, int u) {
+    l.data[l.size] = u;
+    l.size++;
+}
+
+void printList(List l) {
+    for (int i = 0; i < l.size; i++) {
+        cout << l.data[i] << " ";
+    }
+    cout << endl;
+}
+
+int popList(List &l) {
+    int u = l.data[l.size - 1];
+    l.size--;
+    return u;
+}
 // Initialize graph
-void initGraph(graph &g) {
+void initGraph(Graph &g) {
     for (int i = 1; i <= g.vertex; i++) {  // Change from 1 to 0-based index
         for (int j = 1; j <= g.vertex; j++) {  // Change from 1 to 0-based index
             g.matrix[i][j] = 0;
@@ -17,18 +44,18 @@ void initGraph(graph &g) {
 }
 
 // Check adjacency between vertices u and v
-int adjacent(graph g, int u, int v) {
+int adjacent(Graph g, int u, int v) {
     return g.matrix[u][v];
 }
 
 // Add an edge between vertices u and v
-void addEdge(graph &g, int u, int v) {
+void addEdge(Graph &g, int u, int v) {
     g.matrix[u][v] = 1;
     g.matrix[v][u] = 1;
 }
 
 // Print the graph's adjacency matrix
-void printGraph(graph g) {
+void printGraph(Graph g) {
     for (int i = 1; i <= g.vertex; i++) {  // Change from 1 to 0-based index
         for (int j = 1; j <= g.vertex; j++) {  // Change from 1 to 0-based index
             cout << g.matrix[i][j] << " ";
@@ -38,14 +65,14 @@ void printGraph(graph g) {
 }
 
 // Create a graph by entering the number of vertices
-void createGraph(graph &g) {
+void createGraph(Graph &g) {
     cout << "Enter number of vertices: ";
     cin >> g.vertex;
     initGraph(g);
 }
 
 // Calculate the degree of a vertex u
-int degree(graph g, int u) {
+int degree(Graph g, int u) {
     int deg = 0;
     for (int i = 1; i <= g.vertex; i++) {  // Change from 1 to 0-based index
         if (g.matrix[u][i] == 1) {
@@ -56,7 +83,7 @@ int degree(graph g, int u) {
 }
 
 // Read graph data from a file
-void readGraph(string filename, graph &g) {
+void readGraph(string filename, Graph &g) {
     FILE *f = fopen(filename.c_str(), "r");
     if (f == NULL) {
         cout << "Can't open file" << endl;
@@ -71,9 +98,21 @@ void readGraph(string filename, graph &g) {
     fclose(f);
 }
 
+List neighbors(Graph g, int u) {
+    List l;
+    initList(l);
+    for (int i = 1; i <= g.vertex; i++) {  // Change from 1 to 0-based index
+        if (g.matrix[u][i] == 1) {
+            addList(l, i);
+        }
+    }
+    
+    return l;
+}
+
 int main() {
     cout << "Hello, World!" << endl;
-    graph g;
+    Graph g;
     createGraph(g);
     printGraph(g);
 
